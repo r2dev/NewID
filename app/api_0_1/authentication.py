09359acf7b068978ac645be658252f8a4e46b7cf
R2 <rren2@myseneca.ca>
2015-07-16 20:17:55 -0400

    auth updated, add contact

--- app/api_0_1/authentication.py
+++ app/api_0_1/authentication.py
@@ -1,24 +1,32 @@
 from flask import jsonify, g
 from flask.ext.httpauth import HTTPBasicAuth
-from flask.ext.login import AnonymousUserMixin
+from flask.ext.login import AnonymousUserMixin, current_user
 from ..models import User
 from . import api
 from .errors import unauthorized, forbidden
+
 auth = HTTPBasicAuth()
 
 @auth.verify_password
 def verify_password(email_or_token, password):
-	user = User.verify_auth_token(email_or_token)
+	if email_or_token == '':
+		g.user = AnonymousUserMixin()
+		return True
+	if password == '':
+		g.current_user = User.verify_auth_token(email_or_token)
+		g.token_used = True
+		return g.current_user is not None
+	user = User.query.filter_by(email=email_or_token).first()
 	if not user:
-		user = User.query.filter_by(email=email_or_token).first()
-		if not user or not user.verify_password(password):
-			return False
-	g.user = user
-	return True
+		return False
+	g.current_user = user
+	g.token_used = False
+	return user.verify_password(password)
+
 @api.before_request
 @auth.login_required
 def before_request():
-	pass
+	g.user = current_user
 
 @auth.error_handler
 def auth_error():
--- app/api_0_1/users.py
+++ app/api_0_1/users.py
@@ -2,12 +2,12 @@ from flask import jsonify, g
 from . import api
 from ..models import User
 from .errors import unauthorized
+from .authentication import auth
 @api.route('/users/<profile_url>')
 def get_user(profile_url):
     user = User.query.filter_by(profile_url=profile_url).first_or_404()
     return jsonify(user.to_json())
 @api.route('/users/')
+@auth.login_required
 def user_profile():
-    if g.current_user.is_anonymous():
-        return unauthorized("Invalid credentials")
     return jsonify(g.current_user.to_json())
